c68173fb7489e5354a20d6d231aaf8707c130aef
yaana <ya.anastasya.b@yandex.ru>
2022-10-22 08:48:13 +0300

    feat: прозрачность кнопки закрытия попап при наведении

--- movies-explorer-frontend/src/components/SearchForm/SearchForm.js
+++ movies-explorer-frontend/src/components/SearchForm/SearchForm.js
@@ -1,13 +1,22 @@
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
+import findButton from '../../images/find-button.png';
 
 function SearchForm() {
   return (
-    <div className="search-form__content">
-      <div className="search-form__search-area">
-        <FilterCheckbox
-          label="Короткометражки"
-        />
-      </div>
+    <div className="search__content">
+      <form className="search__form">
+        <div className="search__input-area">
+          <input type="text" className="search__form-input" placeholder="Фильм" />
+          <button type="submit" className="search__form-button">
+            <img className="search__form-button-img" src={findButton} alt="найти" />
+          </button>
+        </div>
+        <div className="search__filter-area">
+          <FilterCheckbox
+            label="Короткометражки"
+          />
+        </div>
+      </form >
     </div>
   );
 }
