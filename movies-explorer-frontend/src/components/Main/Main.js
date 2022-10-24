c8bd38764874dbde623fe077dcec8c5e7b43369d
yaana <ya.anastasya.b@yandex.ru>
2022-10-24 15:52:57 +0300

    refactor: Портфолио перенесено в Portfolio.js

--- movies-explorer-frontend/src/components/Main/Main.js
+++ movies-explorer-frontend/src/components/Main/Main.js
@@ -3,6 +3,7 @@ import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
   return (
@@ -12,6 +13,7 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
     </>
   )
 }
---
+++ movies-explorer-frontend/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,30 @@
+function Portfolio() {
+  return (
+    <section className='portfolio'>
+      <h3 className='portfolio__title'>
+        Портфолио
+      </h3>
+      <nav>
+        <ul className='portfolio__links'>
+          <li>
+            <a className="portfolio__link portfolio__link_underlined" target="_blank" rel="noreferrer" href="https://github.com/yaanab/how-to-learn">
+              Статичный сайт
+            </a>
+          </li>
+          <li>
+            <a className="portfolio__link portfolio__link_underlined" target="_blank" rel="noreferrer" href="https://yaanab.github.io/russian-travel/index.html">
+              Адаптивный сайт
+            </a>
+          </li>
+          <li>
+            <a className="portfolio__link" target="_blank" rel="noreferrer" href="https://yaana.nomoredomains.sbs">
+              Одностраничное приложение
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
+
+export default Portfolio;
